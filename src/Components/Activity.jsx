045dda1c393e1af5072d3ce2e9753bd105184140
TimmyOGH <paingphyomyatdin@gmail.com>
2025-01-16 18:08:36 +0700

    Initial commit

---
+++ src/Components/Activity.jsx
@@ -0,0 +1,197 @@
+import React from "react";
+
+function Activity() {
+  return (
+    <section id="activity">
+      {/* Title */}
+      <div className="w-fit mx-auto text-3xl text-center font-bold my-6">
+        <h2>Activities</h2>
+        <div className="border-b-4 border-black mt-2 mx-6"></div>
+      </div>
+
+      {/* Grid Container */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-5 pb-0 text-xl">
+        {/* Column 1 */}
+        <div className="space-y-6">
+          <div>
+            <img
+              src="/activity_gallery/ag_1.jpeg"
+              alt="Activity 1"
+              className="w-full mb-6"
+            />
+            <span>15.01.25</span>
+            <p className="text-gray-700 mt-2 mb-24">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              tempus sapien lacus, in fringilla tortor scelerisque et. Duis
+              laoreet felis non sem imperdiet malesuada non vitae metus. Sed
+              nulla nulla, hendrerit a efficitur vel, porta auctor nibh. Nam non
+              vehicula arcu. In facilisis semper est, sed facilisis sem. Aliquam
+              lacinia hendrerit posuere. <br />
+              <br /> Fusce tincidunt eget sem id sodales. Integer in cursus
+              diam, ac sollicitudin turpis. Suspendisse vitae sem est. Nullam ut
+              convallis diam, vitae consectetur metus. Vivamus quis dolor sed
+              massa ornare eleifend. Nunc condimentum nulla at sapien sodales
+              commodo. Quisque a efficitur tortor, ut posuere orci. Etiam libero
+              eros, vulputate a urna in, aliquet suscipit dolor. Donec eu
+              tincidunt urna.
+            </p>
+          </div>
+          <div>
+            <img
+              src="/activity_gallery/ag_4.jpeg"
+              alt="Activity 2"
+              className="w-full mb-6"
+            />
+            <span>15.01.25</span>
+            <p className="text-gray-700 mt-2 mb-24">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              tempus sapien lacus, in fringilla tortor scelerisque et. Duis
+              laoreet felis non sem imperdiet malesuada non vitae metus. Sed
+              nulla nulla, hendrerit a efficitur vel, <br />
+              <br /> porta auctor nibh. Nam non vehicula arcu. In facilisis
+              semper est, sed facilisis sem. Aliquam lacinia hendrerit posuere.
+              Fusce tincidunt eget sem id sodales. Integer in cursus diam, ac
+              sollicitudin turpis.
+            </p>
+          </div>
+          <div className="sm:block lg:hidden hidden">
+            <img
+              src="/activity_gallery/ag_3.jpeg"
+              alt="Activity 5"
+              className="w-full mb-6"
+            />
+            <span>15.01.25</span>
+            <p className="text-gray-700 mt-2 mb-24">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              tempus sapien lacus, in fringilla tortor scelerisque et. Duis
+              laoreet felis non sem imperdiet malesuada non vitae metus. Sed
+              nulla nulla, hendrerit a efficitur vel, porta auctor nibh. Nam non
+              vehicula arcu. In facilisis semper est, <br />
+              <br /> sed facilisis sem. Aliquam lacinia hendrerit posuere. Fusce
+              tincidunt eget sem id sodales. Integer in cursus diam, ac
+              sollicitudin turpis. Suspendisse vitae sem est. Nullam ut
+              convallis diam, vitae consectetur metus. Vivamus quis dolor sed
+              massa ornare eleifend. Nunc condimentum nulla at sapien sodales
+              commodo. Quisque a efficitur tortor, ut posuere orci. Etiam libero
+              eros, vulputate a urna in, aliquet suscipit dolor. Donec eu
+              tincidunt urna.
+            </p>
+          </div>
+        </div>
+
+        {/* Column 2 */}
+        <div className="space-y-6">
+          <div>
+            <img
+              src="/activity_gallery/ag_2.jpeg"
+              alt="Activity 3"
+              className="w-full mb-6"
+            />
+            <span>15.01.25</span>
+            <p className="text-gray-700 mt-2 mb-24">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              tempus sapien lacus, in fringilla tortor scelerisque et. Duis
+              laoreet felis non sem imperdiet malesuada non vitae metus. Sed
+              nulla nulla, hendrerit a efficitur vel, porta auctor nibh. Nam non
+              vehicula arcu. In facilisis semper est, sed facilisis sem. Aliquam
+              lacinia hendrerit posuere. <br />
+              <br /> Fusce tincidunt eget sem id sodales. Integer in cursus
+              diam, ac sollicitudin turpis. Suspendisse vitae sem est. Nullam ut
+              convallis diam, vitae consectetur metus. Vivamus quis dolor sed
+              massa ornare eleifend. Nunc condimentum nulla at sapien sodales
+              commodo. Quisque a efficitur tortor, ut posuere orci. Etiam libero
+              eros, vulputate a urna in, aliquet suscipit dolor. Donec eu
+              tincidunt urna.
+            </p>
+          </div>
+          <div>
+            <img
+              src="/activity_gallery/ag_5.jpeg"
+              alt="Activity 4"
+              className="w-full mb-6"
+            />
+            <span>15.01.25</span>
+            <p className="text-gray-700 mt-2 mb-24">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              tempus sapien lacus, in fringilla tortor scelerisque et. Duis
+              laoreet felis non sem imperdiet malesuada non vitae metus. Sed
+              nulla nulla, hendrerit a efficitur vel, <br />
+              <br /> porta auctor nibh. Nam non vehicula arcu. In facilisis
+              semper est, sed facilisis sem. Aliquam lacinia hendrerit posuere.
+              Fusce tincidunt eget sem id sodales. Integer in cursus diam, ac
+              sollicitudin turpis. Suspendisse vitae sem est. Nullam ut
+              convallis diam, vitae consectetur metus.
+            </p>
+          </div>
+          <div className="sm:block lg:hidden hidden">
+            <img
+              src="/activity_gallery/ag_6.jpeg"
+              alt="Activity 6"
+              className="w-full mb-6"
+            />
+            <span>15.01.25</span>
+            <p className="text-gray-700 mt-2 mb-24">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              tempus sapien lacus, in fringilla tortor scelerisque et. Duis
+              laoreet felis non sem imperdiet malesuada non vitae metus. Sed
+              nulla nulla, <br />
+              <br /> hendrerit a efficitur vel, porta auctor nibh. Nam non
+              vehicula arcu. In facilisis semper est, sed facilisis sem. Aliquam
+              lacinia hendrerit posuere. Fusce tincidunt eget sem id sodales.
+              Integer in cursus diam, ac sollicitudin turpis. Suspendisse vitae
+              sem est. Nullam ut convallis diam, vitae consectetur metus.
+            </p>
+          </div>
+        </div>
+
+        {/* Column 3 */}
+        <div className="space-y-6 lg:block sm:hidden">
+          <div>
+            <img
+              src="/activity_gallery/ag_3.jpeg"
+              alt="Activity 5"
+              className="w-full mb-6"
+            />
+            <span>15.01.25</span>
+            <p className="text-gray-700 mt-2 mb-24">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              tempus sapien lacus, in fringilla tortor scelerisque et. Duis
+              laoreet felis non sem imperdiet malesuada non vitae metus. Sed
+              nulla nulla, hendrerit a efficitur vel, porta auctor nibh. Nam non
+              vehicula arcu. In facilisis semper est, <br />
+              <br /> sed facilisis sem. Aliquam lacinia hendrerit posuere. Fusce
+              tincidunt eget sem id sodales. Integer in cursus diam, ac
+              sollicitudin turpis. Suspendisse vitae sem est. Nullam ut
+              convallis diam, vitae consectetur metus. Vivamus quis dolor sed
+              massa ornare eleifend. Nunc condimentum nulla at sapien sodales
+              commodo. Quisque a efficitur tortor, ut posuere orci. Etiam libero
+              eros, vulputate a urna in, aliquet suscipit dolor. Donec eu
+              tincidunt urna.
+            </p>
+          </div>
+          <div>
+            <img
+              src="/activity_gallery/ag_6.jpeg"
+              alt="Activity 6"
+              className="w-full mb-6"
+            />
+            <span>15.01.25</span>
+            <p className="text-gray-700 mt-2 mb-24">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              tempus sapien lacus, in fringilla tortor scelerisque et. Duis
+              laoreet felis non sem imperdiet malesuada non vitae metus. Sed
+              nulla nulla, <br />
+              <br /> hendrerit a efficitur vel, porta auctor nibh. Nam non
+              vehicula arcu. In facilisis semper est, sed facilisis sem. Aliquam
+              lacinia hendrerit posuere. Fusce tincidunt eget sem id sodales.
+              Integer in cursus diam, ac sollicitudin turpis. Suspendisse vitae
+              sem est. Nullam ut convallis diam, vitae consectetur metus.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Activity;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,35 +2,31 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-100 py-4 px-12 flex justify-between items-center sticky top-0">
+    <nav className="bg-white py-4 px-12 flex justify-between items-center sticky top-0">
       <div className="text-white text-lg font-bold flex items-center space-x-2">
         <a href="/">
           <img src="/2FLogo.png" alt="2FLogo" width="50px" />
         </a>
         <img src="/2FText.png" alt="2FText" width="120px" />
       </div>
-      <ul className="flex space-x-24 text-xl font-extrabold italic">
-        <li>
-          <a href="/home" className="hover:text-blue-400">
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="/gallery" className="hover:text-blue-400">
-            Gallery
-          </a>
-        </li>
-        <li>
-          <a href="/activities" className="hover:text-blue-400">
-            Activities
-          </a>
-        </li>
-        <li>
-          <a href="/contact" className="hover:text-blue-400">
-            Contact
-          </a>
-        </li>
-      </ul>
+      <div className="hidden h-10 text-2xl font-medium md:flex md:space-x-8">
+        <div className="group">
+          <a href="#">Home</a>
+          <div className="mx-2 group-hover:border-b-2 group-hover:border-black"></div>
+        </div>
+        <div className="group">
+          <a href="#gallery">Gallery</a>
+          <div className="mx-2 group-hover:border-b-2 group-hover:border-black"></div>
+        </div>
+        <div className="group">
+          <a href="#activity">Activities</a>
+          <div className="mx-2 group-hover:border-b-2 group-hover:border-black"></div>
+        </div>
+        <div className="group">
+          <a href="#footer">Contact</a>
+          <div className="mx-2 group-hover:border-b-2 group-hover:border-black"></div>
+        </div>
+      </div>
     </nav>
   );
 };
