ca68c42be48a49bcd2aa139cfd3fd5fbcde8b0a6
Auratha <auratha1000@gmail.com>
2025-01-27 17:48:45 +0700

    change gallery section to left and right scroll the img, and add google map in Acitivity below

--- src/Components/Activity.jsx
+++ src/Components/Activity.jsx
@@ -189,6 +189,17 @@ function Activity() {
           </div>
         </div>
       </div>
+
+      <h4 className="mb-4 text-4xl text-center">Our Location</h4>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3776.666123228664!2d99.01509257497044!3d18.81302698233816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30da25323ca4b707%3A0xb4df958552bccb5a!2sChiang%20Mai%20CCC%20Center!5e0!3m2!1smy!2sth!4v1737818191784!5m2!1smy!2sth"
+        className="w-[90%] mx-auto"
+        height={450}
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
     </section>
   );
 }
--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,94 +1,42 @@
-import { useEffect, useRef } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-const Gallery = () => {
-  const galleryRef = useRef(null);
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-  //This is for pc mouse-scroll
-  const handleScroll = (event) => {
-    const gallery = galleryRef.current;
-    const maxScrollLeft = gallery.scrollWidth - gallery.clientWidth;
-    const scrollSpeed = 5; // Adjust this value to increase or decrease scroll speed
+import "./gallery.css";
 
-    if (event.deltaY !== 0) {
-      if (gallery.scrollLeft + event.deltaY * scrollSpeed >= maxScrollLeft) {
-        gallery.scrollLeft = maxScrollLeft;
-        event.preventDefault();
-        // Scroll to the below section
-        window.scrollTo({
-          top: gallery.getBoundingClientRect().bottom + window.scrollY,
-          behavior: "smooth",
-        });
-      } else if (gallery.scrollLeft + event.deltaY * scrollSpeed <= 0) {
-        gallery.scrollLeft = 0;
-        event.preventDefault();
-        // Scroll to the top of the section
-        window.scrollTo({
-          top: 0,
-          behavior: "smooth",
-        });
-      } else {
-        gallery.scrollLeft += event.deltaY * scrollSpeed;
-        event.preventDefault();
-      }
-    }
-  };
+// import required modules
+import { Pagination, Navigation } from "swiper/modules";
 
-  //This is for mobile touch-scroll
-  const handleTouchStart = (event) => {
-    const touch = event.touches[0];
-    galleryRef.current.startX = touch.clientX;
-  };
-
-  const handleTouchMove = (event) => {
-    const touch = event.touches[0];
-    const gallery = galleryRef.current;
-    const maxScrollLeft = gallery.scrollWidth - gallery.clientWidth;
-    const scrollSpeed = 50; // Increase this value to make the touch movement more responsive
-
-    const deltaX = gallery.startX - touch.clientX;
-    gallery.startX = touch.clientX;
-
-    if (gallery.scrollLeft + deltaX * scrollSpeed >= maxScrollLeft) {
-      gallery.scrollLeft = maxScrollLeft;
-      event.preventDefault();
-    } else if (gallery.scrollLeft + deltaX * scrollSpeed <= 0) {
-      gallery.scrollLeft = 0;
-      event.preventDefault();
-    } else {
-      gallery.scrollLeft += deltaX * scrollSpeed;
-      event.preventDefault();
-    }
-  };
-
-  useEffect(() => {
-    const galleryContainer = galleryRef.current;
-    galleryContainer.addEventListener("wheel", handleScroll);
-    galleryContainer.addEventListener("touchstart", handleTouchStart);
-    galleryContainer.addEventListener("touchmove", handleTouchMove);
-    return () => {
-      galleryContainer.removeEventListener("wheel", handleScroll);
-      galleryContainer.removeEventListener("touchstart", handleTouchStart);
-      galleryContainer.removeEventListener("touchmove", handleTouchMove);
-    };
-  }, []);
-
+export default function Gallery() {
   return (
-    <section id="gallery">
-      <div
-        ref={galleryRef}
-        className="gallery-container w-full h-[350px] sm-[450px] md:h-[500px] lg:h-[700px] overflow-hidden scroll-smooth flex flex-shrink-0"
+    <div id="gallery">
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper h-[300px] sm:h-[400px] md:h-[500px] lg:h-[700px]"
       >
-        {[...Array(4)].map((_, index) => (
-          <img
-            key={index}
-            src={`/gallery/gallery${index + 1}.jpg`}
-            alt={`gallery${index + 1}`}
-            className="min-w-full object-cover"
-          />
-        ))}
-      </div>
-    </section>
+        {[...Array(4)].map((_, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <img
+                src={`./gallery/gallery${index + 1}.jpg`}
+                alt="2f gallery"
+                className="w-full h-full object-cover"
+              />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
   );
-};
-
-export default Gallery;
+}
